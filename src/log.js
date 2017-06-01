3ee4bcd79619ec0b5d5b70a5b1d5bfa6f9999d08
differui <differui@gmail.com>
2017-06-01 16:30:50 +0800

    Add method into log message

--- src/log.js
+++ src/log.js
@@ -2,9 +2,9 @@ import { resolve } from 'path'
 import { green, gray, white } from 'colors'
 
 export function mock(req) {
-  console.log(green(`Mock: ${resolve('/', req.url)}`))
+  console.log(green(`Mock: ${req.method} ${resolve('/', req.url)}`))
 }
 
 export function proxy(req) {
-  console.log(gray(`Proxy: ${resolve('/', req.url)}`))
+  console.log(gray(`Proxy: ${req.method} ${resolve('/', req.url)}`))
 }
